8fd0df55e0a15c99a7633dcba6ad53c3cc9fd17a
Peter Zhang <pwzhang@crimson.ua.edu>
2022-01-20 19:34:33 -0600

    update comment convention

--- numbergame.lisp
+++ numbergame.lisp
@@ -1,16 +1,19 @@
+;;;;
+;;; you can load this file by pasting the following into CLISP:
+;;; (load "numbergame.lisp")
 (defparameter *small* 1); global var/ top level definition
 (defparameter *big* 100); ** are called earmuffers, lispers like to make global variables with these
-; defvar will not overwrite a value but defparameter will
+;;; defvar will not overwrite a value but defparameter will
 
-; spaces and line breaks are ignored
+;;; spaces and line breaks are ignored
 
-; defun will return the name of the newly created global function
+;;; defun will return the name of the newly created global function
 (defun guess-my-number ()
     (ash (+ *small* *big*) -1)); ash is arithmetic shift which will shift it by the second parameter
-; final value calculated in body is returned automatically
+;;; final value calculated in body is returned automatically
 
-; setf sets the value of variable
-; 1- subtracts 1 to value, 1+ adds 1 to value
+;;; setf sets the value of variable
+;;; 1- subtracts 1 to value, 1+ adds 1 to value
 (defun smaller ()
     (setf *big* (1- (guess-my-number)))
     (guess-my-number))
